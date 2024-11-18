afa3811f135e5aa99895286521c3007451cbd074
omarsaeed7 <omarbensaeed@proton.me>
2024-11-18 14:47:48 +0200

    modify

--- dwm/config.h
+++ dwm/config.h
@@ -28,8 +28,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "TelegramDesktop",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "TelegramDesktop",  NULL,       NULL,       1 << 3,       0,           -1 },	
+	/*{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },*/
 };
 /*
  * Power Things 
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "kitty", NULL }; /*  kitty, NULL */
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
